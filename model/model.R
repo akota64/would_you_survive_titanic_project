f11b5ee3d6820f80c71a24669c72d8b730e269be
akota64 <akhil.kota37@gmail.com>
2022-08-07 17:07:46 -0400

    Final model generated and saved in app folder

--- model/model.R
+++ model/model.R
@@ -2,10 +2,14 @@
 #' Final model is saved as an RDS in the Shiny project directory (app)
 #' See the accompanying .Rmd for initial pre-processing and model testing with cross validation.
 
-## Load data
-dat <- read_csv("../data/train.csv")
+library(tidyverse)
+library(caret)
+
+## Load data, make sure wd is set to the directory containing data
+dat <- read_csv("data/train.csv")
 
 ## Remove undesired columns and transform categorical columns to factor
+## Make dummy vars out of Embarked
 train <- dat %>%
     select(-PassengerId, -Name, -Cabin, -Ticket) %>%
     mutate(
@@ -14,22 +18,29 @@ train <- dat %>%
         Sex = factor(Sex),
         Pclass = factor(Pclass)
     )
+dmy <- dummyVars(" ~ Embarked", train)
+dv <- data.frame(predict(dmy, train))
+dv[is.na(dv)]<-0
+train <- train %>% 
+    select(-Embarked) %>%
+    mutate(
+        Cherbourg = as.factor(dv$Embarked.C), 
+        Queenstown = as.factor(dv$Embarked.Q),
+        Southampton = as.factor(dv$Embarked.S)
+    )
 
-## Impute NA's in Age column with KNN imputation
+## Impute NA's with KNN imputation
 set.seed(64)
-tf <- as.data.frame(select(train, -Survived))
-Survived <- train$Survived
-
-knni <- preProcess(tf, method = "knnImpute")
-ptf <- predict(knni, tf)
-
-ptrain <- cbind(ptf, Survived)
-
-## Remove rows with Embarked = NA (only 2)
-ptrain <- ptrain[which(!is.na(ptrain$Embarked)),]
+train <- as.data.frame(train)
+knn <- preProcess(train[,-1], method = "knnImpute")
+train[,-1] <- predict(knn, train[,-1])
 
 ## Build model
 set.seed(64)
-model <- train(Survived~., data=ptrain, method="rf")
+model <- train(Survived~., data=train, method="rf")
 
-## Save model. One in app folder and one in 
+## Save two copies of the model. One in app folder and one in model folder
+## Note that we will not need to KNN impute in the app, since it will be impossible to do so
+## The app will feed the necessary data to the model in exactly the right format
+saveRDS(model, "app/final_model.rds")
+saveRDS(model, "model/final_model.rds")
